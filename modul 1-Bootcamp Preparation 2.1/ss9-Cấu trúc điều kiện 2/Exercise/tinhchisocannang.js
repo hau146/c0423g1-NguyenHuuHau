566ed37e89042a6a26fc687983864bf704955a8a
hau146 <131940769+hau146@users.noreply.github.com>
2023-05-12 20:19:30 +0700

    ok

---
+++ modul 1-Bootcamp Preparation 2.1/ss9-Cấu trúc điều kiện 2/Exercise/tinhchisocannang.js
@@ -0,0 +1,13 @@
+function check() {
+    let weight = document.getElementById("kg").value;
+    let height = document.getElementById("m").value;
+    let bmi = weight / (height * height);
+    if (bmi < 18) {
+        document.getElementById("ketqua").innerText = 'chỉ số của bạn là :' + bmi + " (bạn quá gầy) " ;
+    } else if (bmi < 25.0) {
+        document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (cân nặng tiêu chuẩn)" ;
+    } else if (bmi < 30.0){
+        document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (Mập quá rồi nha)";
+}else
+        document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (Bạn bị béo phì quá mức, đến lúc tập thể dục)";
+}
