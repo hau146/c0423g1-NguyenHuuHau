d5d470725e60ce96d281b71676aef1f2e592d36e
hau146 <131940769+hau146@users.noreply.github.com>
2023-05-16 20:39:37 +0700

    okk

--- modul 1-Bootcamp Preparation 2.1/ss9-Cấu trúc điều kiện 2/Exercise/Tính chỉ số cân nặng/tinhchisocannang.js
+++ modul 1-Bootcamp Preparation 2.1/ss9-Cấu trúc điều kiện 2/Exercise/Tính chỉ số cân nặng/tinhchisocannang.js
@@ -3,10 +3,10 @@ function check() {
     let height = document.getElementById("m").value;
     let bmi = weight / (height * height);
     if (bmi < 18) {
-        document.getElementById("ketqua").innerText = 'chỉ số của bạn là :' + bmi + " (bạn quá gầy) " ;
-    } else if (bmi < 25.0) {
+        document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là :' + bmi + " (bạn quá gầy) " ;
+    } else if (bmi < 25.0 && bmi >= 18) {
         document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (cân nặng tiêu chuẩn)" ;
-    } else if (bmi < 30.0){
+    } else if (bmi < 30.0 && bmi >= 25){
         document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (Mập quá rồi nha)";
 }else
         document.getElementById("ketqua").innerHTML = 'chỉ số của bạn là : ' + bmi + " (Bạn bị béo phì quá mức, đến lúc tập thể dục)";
